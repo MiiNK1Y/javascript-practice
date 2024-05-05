1c7a82ff5d23ee08a88977b122054c1f2ba5b9cb
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2024-05-05 22:58:25 +0200

    formating

--- javascript_from_beginner_to_professional/rockPaperScissorsGame/app.js
+++ javascript_from_beginner_to_professional/rockPaperScissorsGame/app.js
@@ -1,15 +1,15 @@
-function createPCPick(){
-	const options = ["rock", "paper", "scissors"];
-	const min = 0;
-	const max = 2;
-	let pcPickOption = Math.floor(Math.random() * (max - min + 1)) + min;
+function createPCPick() {
+    const options = ["rock", "paper", "scissors"];
+    const min = 0;
+    const max = 2;
+    let pcPickOption = Math.floor(Math.random() * (max - min + 1)) + min;
 
-	return String(options[pcPickOption]);
+    return String(options[pcPickOption]);
 }
 
-function clientPick(pick){
-	document.getElementById("user-pick-window").innerHTML = pick;
-}
+function rollGame() {}
 
-function rollGame(){
+function clientPick(pick) {
+    document.getElementById("user-pick-window").innerHTML = pick;
+    rollGame();
 }
