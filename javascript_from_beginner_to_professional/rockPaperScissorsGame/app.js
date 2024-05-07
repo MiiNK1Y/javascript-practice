2b7cecd2b293c570e36b46b7c86286e811590fc9
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2024-05-07 19:16:32 +0200

    made winner-selection work

--- javascript_from_beginner_to_professional/rockPaperScissorsGame/app.js
+++ javascript_from_beginner_to_professional/rockPaperScissorsGame/app.js
@@ -1,4 +1,4 @@
-function createPCPick() {
+function createPcPick() {
     const options = ["rock", "paper", "scissors"];
     const min = 0;
     const max = 2;
@@ -7,25 +7,44 @@ function createPCPick() {
     return String(options[pcPickOption]);
 }
 
-
-
-function rollGame(userPick) {
-    pcPick = createPCPick();
-    document.getElementById("pc-pick-window").innerHTML = pcPick;
-    switch (userPick) {
-        case (userPick == pcPick):
-            console.log("same pick, roll again");
-            break;
-        case (userPick == "scissors") && (pcPick == "rock"):
-            console.log("PC chose rock, and wins!");
-            break;
-        default:
-            console.log("undecided");
-            break;
+function userWin(userPick, pcPick) {
+    if (
+        userPick === "rock" && pcPick === "scissors" ||
+        userPick === "paper" && pcPick === "rock" ||
+        userPick === "scissors" && pcPick === "paper"
+    ) {
+        return true;
+    }
+    else if (userPick == pcPick) {
+        return null;
+    }
+    else {
+        return false;
     }
 }
 
-function clientPick(pick) {
-    document.getElementById("user-pick-window").innerHTML = pick;
-    rollGame(pick);
+function rollGame(userPick) {
+    pcPick = createPcPick();
+
+    console.log(`pc picks: ${pcPick}`); // log
+
+    document.getElementById("pc-pick-window").innerHTML = pcPick;
+    winner = userWin(userPick, pcPick);
+    if (winner) {
+        console.log("user wins");
+    }
+    else if (winner == null) {
+        console.log("no winner, roll again.");
+    }
+    else {
+        console.log("pc wins");
+    }
+}
+
+function clientPick(option) {
+    document.getElementById("user-pick-window").innerHTML = option;
+
+    console.log(`user picks: ${option}`); // log
+
+    rollGame(option);
 }
